36b31bd8fc60651cd1fcdf7f3c713c9b256e5f4b
Bolla Sharvani <170030154@kluniversity.in>
2021-07-05 19:06:42 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import {Container, Button, Form} from 'react-bootstrap';
 import Amplify from "aws-amplify";
 import { API } from 'aws-amplify';
 
+import { useLocation } from 'react-router-dom';
+import queryString from query-String;
+
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
@@ -30,6 +33,9 @@ function updateFormState(key, value) {
 }
 
 function App() {
+  const { search } = useLocation();
+  console.log(search);
+  const { firstname, lastname, email } = queryString.parse(search);
   return (
     <Container>
     <div>
@@ -38,15 +44,15 @@ function App() {
         <Form>
           <Form.Group>
             <Form.Label>FirstName</Form.Label>
-            <Form.Control placeholder="FirstName" onChange={e => updateFormState('FirstName', e.target.value)} />
+            <Form.Control placeholder="FirstName" value = {firstname} onChange={updateFormState('FirstName', firstname)} />
           </Form.Group>
           <Form.Group>
             <Form.Label>LastName</Form.Label>
-            <Form.Control placeholder="LastName" onChange={e => updateFormState('LastName', e.target.value)} />
+            <Form.Control placeholder="LastName" value = {firstname} onChange={updateFormState('LastName', lastname)} />
           </Form.Group>
           <Form.Group>
             <Form.Label>MailId</Form.Label>
-            <Form.Control placeholder="MailId" onChange={e => updateFormState('MailId', e.target.value)} />
+            <Form.Control placeholder="MailId" value = {email} onChange={updateFormState('MailId', email)} />
           </Form.Group>
           <Form.Group>
             <Form.Label>FeedbackMessage</Form.Label>
